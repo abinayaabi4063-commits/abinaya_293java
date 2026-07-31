9e3a54c38e021755e24aa4fe8c5679d996efca27
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-07-31 12:09:59 +0530

    add

--- task1.java
+++ task1.java
@@ -1,6 +1,17 @@
 class task1{
     public static void main (String args[]){
-        System.out.println("welcome dude");
+       String name ="abinaya";
+       system.out.println("name::name");
 
     }
 }
+
+  // String Name ="abinaya";
+        // long regNumber = 620622;
+        // float cgpa = 7.5f;
+        // char attendence = 'P';
+
+        // System.out.println("Name : "+Name);
+        // System.out.println("reg Number : "+regNumber);
+        // System.out.println("CGPA : "+cgpa);
+        // System.out.println("Attendence : "+attendence);
